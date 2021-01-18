5d910cbc239ba631b80a2cb2edcf01982743fe3e
nicola.munz <nmunz@hotmail.com>
2021-01-18 16:43:19 +0100

    prototype of board generating

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { PrimaryButton, TextField } from "@fluentui/react";
+import { Square } from "./Square";
 
 function App() {
+  const [size, setSize] = useState(2);
+  const [squares, setSquares] = useState<{ x: number; y: number }[]>([
+    { x: 1, y: 1 },
+    { x: 2, y: 1 },
+    { x: 1, y: 2 },
+    { x: 2, y: 2 },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextField
+        label="Size"
+        onChange={(e, str) => {
+          setSize(Number(str ?? "2"));
+        }}
+        value={String(size)}
+      />
+      <PrimaryButton
+        onClick={(e) => {
+          let tmp = new Array<{
+            x: number;
+            y: number;
+          }>();
+          for (let i = 1; i <= size; i++) {
+            for (let j = 1; j <= size; j++) {
+              tmp.push({ x: i, y: j });
+            }
+          }
+          setSquares(tmp);
+        }}
+      />
+      <div className="Grid">
+        {squares.map(({ x, y }) => (
+          <Square x={x} y={y} />
+        ))}
+      </div>
     </div>
   );
 }
